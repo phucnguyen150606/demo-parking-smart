6a2338c2fdee89ae3d7398dbc1b10f8b40c82423
phucnguyen150606 <phucnguyen150606@gmail.com>
2025-12-11 18:25:52 +0700

    clients

--- clients/Main_UI/main.js
+++ clients/Main_UI/main.js
@@ -1,14 +1,9 @@
 function openPage(page) {
     window.location.href = page;
 }
-let index = 0;
-const slides = document.querySelectorAll(".slide");
-
-function showSlide() {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
-    index = (index + 1) % slides.length;
-}
-
-showSlide();
-setInterval(showSlide, 3000); // 3 giây đổi ảnh
+var swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+});
